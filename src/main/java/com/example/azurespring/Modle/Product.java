4bde4e556af196554d9104dd37ee633284bd4f3e
Arkitekt-s <sfattahieh@gmail.com>
2023-01-16 14:49:33 +0100

    Initial commit

---
+++ src/main/java/com/example/azurespring/Modle/Product.java
@@ -0,0 +1,41 @@
+package com.example.azurespring.Modle;
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    //access level is set to NONE, so that the id cannot be changed
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private Integer price;
+
+    @Column(nullable = false, length = 100)
+    private Integer weight;
+
+    //cascade type persist means that if a product is deleted, all the orders that contain that product will be deleted as well
+    //different between cascade type all and persist is that all will delete all the orders that contain that product, but persist will only delete the orders that contain that product
+
+}
---
+++ src/main/java/com/example/azurespring/Repo/ProductRepo.java
@@ -0,0 +1,16 @@
+package com.example.azurespring.Repo;
+
+import com.example.azurespring.Modle.Product;
+import org.hibernate.sql.Select;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ProductRepo extends JpaRepository<Product, Long> {
+    //find product by name
+    //%before and after ?1 can bring all search based on first alphabet
+    @Query(value= "SELECT p From Product p WHERE p.name Like %?1%")
+    List<Product> findByName(String keyword);
+
+}
